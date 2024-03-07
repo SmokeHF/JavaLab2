96ea6489fe1ebc2f3fb4e08fedfad2fe07ab2634
tmarczyn <01161952@pw.edu.pl>
2024-03-07 22:42:17 +0100

    Missing bracket

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,21 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        float wagaMarczynski=67;
-        float wagaMarsMarczynski=wagaMarczynski*(float)0.38;
-        double wagaDoubleMarczynski=wagaMarsMarczynski;
-        int wagaIntMarczynski=(int)wagaDoubleMarczynski;
-        char wagaCharMarczynski=(char)wagaIntMarczynski;
-        int wagaAfterCalculationsMarczynski=wagaCharMarczynski+10;
-        System.out.println("Normal weight: "+wagaMarczynski);
-        System.out.println("Weight on mars: "+wagaMarsMarczynski);
-        System.out.print("Weight with precision to 2 digits: ");
-        System.out.printf("%.2f",wagaDoubleMarczynski);
-        System.out.println();
-        System.out.println("Weight (int): "+wagaIntMarczynski);
-        System.out.println("Weight (char): "+wagaCharMarczynski);
-        System.out.println("Weight after adding 10: "+wagaAfterCalculationsMarczynski);
+        {
+            float wagaMarczynski=67;
+            float wagaMarsMarczynski=wagaMarczynski*(float)0.38;
+            double wagaDoubleMarczynski=wagaMarsMarczynski;
+            int wagaIntMarczynski=(int)wagaDoubleMarczynski;
+            char wagaCharMarczynski=(char)wagaIntMarczynski;
+            int wagaAfterCalculationsMarczynski=wagaCharMarczynski+10;
+            System.out.println("Normal weight: "+wagaMarczynski);
+            System.out.println("Weight on mars: "+wagaMarsMarczynski);
+            System.out.print("Weight with precision to 2 digits: ");
+            System.out.printf("%.2f",wagaDoubleMarczynski);
+            System.out.println();
+            System.out.println("Weight (int): "+wagaIntMarczynski);
+            System.out.println("Weight (char): "+wagaCharMarczynski);
+            System.out.println("Weight after adding 10: "+wagaAfterCalculationsMarczynski);
+        }
     }
 }
